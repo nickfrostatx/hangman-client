f2a094a192452f34d9ddcd95bdb24f371708ff58
Nick Frost <nickfrostatx@gmail.com>
2015-02-12 23:54:33 -0800

    Disable input on move

--- Game.java
+++ Game.java
@@ -10,6 +10,7 @@ public class Game {
     public void move(int space) {
         this.spaces[space] = 1;
         this.window.setSpaceColor(space, 1);
+        this.window.setInputEnabled(false);
         System.out.println("Moved " + space);
     }
 }
